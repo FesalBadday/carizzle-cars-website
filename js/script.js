c2573dcd8f97d373f1d372ff26449ee13329811d
FesalBadday <fesal.n04@hotmail.com>
2021-10-03 11:21:24 -0600

    updated hamburger menu

--- js/script.js
+++ js/script.js
@@ -6,11 +6,11 @@ function openNav() {
   const menutoggle = document.getElementById("menu-toggle");
   menutoggle.style.display = "none";
 
-  const Headhero = document.getElementById("ho-hero");
+  /* const Headhero = document.getElementById("ho-hero");
   Headhero.style.top = "65%";
 
   const heroSec = document.getElementById("ho-hero-sec");
-  heroSec.style.height = "155vh";
+  heroSec.style.height = "155vh"; */
 }
 
 function closeNav() {
@@ -20,9 +20,9 @@ function closeNav() {
   const menutoggle = document.getElementById("menu-toggle");
   menutoggle.style.display = "flex";
 
-  const Headhero = document.getElementById("ho-hero");
+  /* const Headhero = document.getElementById("ho-hero");
   Headhero.style.top = "39%";
 
   const heroSec = document.getElementById("ho-hero-sec");
-  heroSec.style.height = "115vh";
+  heroSec.style.height = "115vh"; */
 }
